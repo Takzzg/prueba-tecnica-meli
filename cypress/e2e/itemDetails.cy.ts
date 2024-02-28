5515d99db8d6218b41cda7d6326f091924c7d703
Takzzg <guido98q@gmail.com>
2024-02-28 13:04:33 -0300

    less fetch requests

--- cypress/e2e/itemDetails.cy.ts
+++ cypress/e2e/itemDetails.cy.ts
@@ -1,28 +1,22 @@
 const randomItemIDs = ['MLA1408167253', 'MLA1129921078', 'MLA1663051160']
 
 describe('Item Details', () => {
-	describe('renders multiple items', () => {
-		for (let index = 0; index < randomItemIDs.length; index++) {
-			const id = randomItemIDs[index]
+	for (let index = 0; index < randomItemIDs.length; index++) {
+		const id = randomItemIDs[index]
 
-			it(id, () => {
+		describe(id, () => {
+			it('renders item', () => {
 				cy.visit('/items/' + id)
 				cy.get('div[data-cy="itemPicture"] > img').should('have.length', 1)
 				cy.get('a[data-cy="buyButton"]').contains('Comprar')
 			})
-		}
-	})
 
-	describe('redirects items to ML', () => {
-		for (let index = 0; index < randomItemIDs.length; index++) {
-			const id = randomItemIDs[index]
-
-			it(id, () => {
+			it('redirects to ML', () => {
 				cy.visit('/items/' + id)
 				cy.get('a[data-cy="buyButton"]')
 					.should('have.attr', 'href')
 					.should('include', id.slice(0, 3) + '-' + id.slice(3))
 			})
-		}
-	})
+		})
+	}
 })
